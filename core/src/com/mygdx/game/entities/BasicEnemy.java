031bc98636f0d4590f16efb4c935b20ac680c812
Laks0 <laksjoaquin@gmail.com>
2020-03-21 14:14:22 -0300

    'AI'

--- core/src/com/mygdx/game/entities/BasicEnemy.java
+++ core/src/com/mygdx/game/entities/BasicEnemy.java
@@ -5,11 +5,29 @@ import com.badlogic.gdx.math.Vector2;
 import world.GameMap;
 
 public class BasicEnemy extends Enemy {
+	private int direction;
+	
 	public BasicEnemy(Vector2 pos) {
-		super(pos, 300f, 200f, "basicEnemy.png");
+		super(pos, 500f, 200f, "basicEnemy.png");
+		
+		direction = 1;
 	}
 	
-	public void update(float dt, GameMap map) {
-		super.update(dt, map);
+	public void update(Player player, float dt, GameMap map) {
+		super.update(player, dt, map);
+		
+		moveX(direction, dt);
+		
+		if (isCollidingX(new Vector2(pos.x + vel.x * dt, pos.y), direction, map)) {
+			direction *= -1;
+		}
+	}
+
+	public int getDirection() {
+		return direction;
+	}
+
+	public void setDirection(int direction) {
+		this.direction = direction;
 	}
 }
--- core/src/com/mygdx/game/levels/Levels.java
+++ core/src/com/mygdx/game/levels/Levels.java
@@ -12,8 +12,8 @@ public enum Levels {
 	TWO(1, new TiledGameMap("level2.tmx"), new Vector2(32, 80)),
 	THREE(2, new TiledGameMap("level3.tmx"), new Vector2(32, 80)),
 	FOUR(3, new TiledGameMap("level4.tmx"), new Vector2(32, 600), new Enemy[]{
-			new BasicEnemy(new Vector2(200, 200)),
-			new BasicEnemy(new Vector2(300,300))
+			new BasicEnemy(new Vector2(200, 432)),
+			new BasicEnemy(new Vector2(500,300))
 	});
 	
 	private GameMap map;
